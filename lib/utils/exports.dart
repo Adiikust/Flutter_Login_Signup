e0a37013720c862f8e5942cae8191d38315a110b
muhammad.adnan <adii.developer277@gmail.com>
2022-02-13 13:13:04 +0500

    seven commit

--- lib/utils/exports.dart
+++ lib/utils/exports.dart
@@ -2,8 +2,8 @@ export 'package:flutter/material.dart';
 
 //Views
 export 'package:login_signup/views/home_screen.dart';
-export 'package:login_signup/views/login_screen.dart';
-export 'package:login_signup/views/signup_sreen.dart';
+export 'package:login_signup/views/login_signup/login_screen.dart';
+export 'package:login_signup/views/login_signup/signup_sreen.dart';
 export 'package:login_signup/views/welcome_Screen.dart';
 
 //Controllers
